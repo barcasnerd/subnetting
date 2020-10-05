ae6753eb7f0b684817ab309b1e585045d991c368
Jair Barcasnegras Estrada <jabares2010@gmail.com>
2020-10-04 20:45:51 -0500

    First version without generator

--- src/public/js/actions.js
+++ src/public/js/actions.js
@@ -1,117 +1,32 @@
-//import html2canvas from 'html2canvas.min.js';
+// Elementos de HTML
+var ip1 = document.getElementById("ip1");
+var ip2 = document.getElementById("ip2");
+var ip3 = document.getElementById("ip3");
+var ip4 = document.getElementById("ip4");
+var nets = document.getElementById("network").value;
+var generateButton = document.getElementById("buttonGenerator");
 
-const htmlTag = document.querySelector('html');
-const bodyTag = document.querySelector('body');
-const myNav = document.querySelector('nav');
-const myElem = document.querySelector('nav li a');
-var button = document.getElementById('responsiveButton');
-var thElements = document.querySelectorAll('th');
-var tdElements = document.querySelectorAll('td');
-var color = document.getElementById('colorChooser');
-var materia = document.getElementById('materia');
-var content = document.getElementById('create');
-var index = 0;
-var id = 0;
-var firstColor = true;
+//Variables de uso
+var ip = [ip1.value, ip2.value, ip3.value, ip4.value];
 
-//Monitoring actions on color chooser
-color.addEventListener('click', () => {
-    firstColor = false;
+//Acciones de eventos
+generateButton.addEventListener('click', () => {
+    generateSubnetting(this.ip, this.nets);
 });
 
-//Adding event listener and first attributes to td elements
-tdElements.forEach((item) => {
-    if (index > 7) {
-        item.setAttribute('style', 'background-color:#202020;');
-        item.setAttribute('class', 'text-center lead');
-        item.setAttribute('id', id++);
-        item.setAttribute('status', '1');
-        item.addEventListener('click', (ev) => {
-            changeColor(item);
-        });
+//Funciones y métodos
+function generateSubnetting(ip, nets) {
+    alert(nets);
+    var sw = verifyValues(ip, nets);
+    if (sw === false) {
+        alert("Campos vacíos");
     }
-    index++;
-});
+};
 
-//listen to actiosn on td elements
-function changeColor(item) {
-    let mate = materia.value;
-    let colo = color.value;
-
-    if (firstColor == true || mate.length == 0 || mate.includes('  ', 0)) {
-        ruleModal.show();
-
-    } else if (item.getAttribute('status') === '1') {
-        if (colo.includes('#ff', 0)) {
-            item.setAttribute('style', 'background-color:' + color.value + ';' + 'color:black;padding:0px;');
-        } else {
-            item.setAttribute('style', 'background-color:' + color.value + ';' + 'color:white;padding:0px;');
-        }
-
-        if (materia.value.length <= 9) {
-            item.innerText = materia.value.toUpperCase();
-        } else {
-            item.innerText = materia.value.substring(0, 6).toUpperCase() + "...";
-        }
-        item.setAttribute('status', '2');
+function verifyValues(ip, n) {
+    if (ip[0].length == 0 || ip[1].length == 0 || ip[2].length == 0 || ip[3].length == 0) {
+        return false;
     } else {
-        item.setAttribute('style', 'background-color:#202020;');
-        item.innerText = ' ';
-        item.setAttribute('status', '1');
+        return true;
     }
-
-}
-
-//addign style to  main th elements
-thElements.forEach((item) => {
-    item.setAttribute('class', 'bg-secondary border-secondary text-light text-center lead');
-});
-
-//instances the modals
-var myModal = new bootstrap.Modal(document.getElementById('instructiveModal'), {
-    keyboard: false
-});
-
-var ruleModal = new bootstrap.Modal(document.getElementById('ruleModal'), {
-    keyboard: false
-});
-
-myModal.show();
-
-//changing background while user is scrolling
-let scrolled = () => {
-    let dec = scrollY / (bodyTag.scrollHeight - innerHeight);
-    return Math.floor(dec * 100);
-}
-
-addEventListener('scroll', () => {
-    myNav.style.setProperty('background', scrolled() > 20 ? "var(--color2)" : "var(--color1");
-});
-
-button.addEventListener('click', () => {
-    myNav.style.setProperty('background', "#000000");
-
-});
-
-
-// instance save and reset buttons
-var save = document.getElementById('saveButton');
-var reset = document.getElementById('resetButton');
-
-//adding reset button actions
-index = 0;
-reset.addEventListener('click', () => {
-    tdElements.forEach((item) => {
-        if (index > 7) {
-            item.setAttribute('style', 'background-color:#202020;');
-            item.innerText = ' ';
-        }
-        index++;
-    });
-});
-
-//adding save button actions
-/*save.addEventListener('click',()=>{
-
-});*/
-
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req,res)=>{
-    res.render('index.html',{title: "Schedule+"});
+    res.render('index.html',{title: "Subnetting"});
 });
 
 module.exports = router;
